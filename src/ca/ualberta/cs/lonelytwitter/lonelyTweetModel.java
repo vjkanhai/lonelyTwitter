5eb404555d92fa38e2b8574995c853cf27f7cdaa
vjkanhai <kanhai@ualberta.ca>
2014-01-14 17:46:25 -0700

    Updated the lonelyTweetModel class.

--- src/ca/ualberta/cs/lonelytwitter/lonelyTweetModel.java
+++ src/ca/ualberta/cs/lonelytwitter/lonelyTweetModel.java
@@ -10,22 +10,30 @@ public class lonelyTweetModel
 	
 		return text;
 	}
-	
 	public void setText(String text)
 	{
 	
 		this.text = text;
 	}
-	
 	public Date getTimestamp()
 	{
 	
 		return timestamp;
 	}
-	
 	public void setTimestamp(Date timestamp)
 	{
-	
 		this.timestamp = timestamp;
+	}
+	public lonelyTweetModel(String text, Date timestamp)
+	{
+		super();
+		this.text = text;
+		this.timestamp = timestamp;
+	}	
+	public lonelyTweetModel(String text)
+	{
+		super();
+		this.text = text;
+		this.timestamp = new Date();
 	}
 }
